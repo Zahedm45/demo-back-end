d68872ceb3c1f4d37b01f2d207870493c628c8f0
Zahed <zahedm45@gmail.com>
2021-06-07 09:46:05 +0200

    User and Game class created

---
+++ src/main/java/com/example/demo/model/adm/User.java
@@ -0,0 +1,6 @@
+package com.example.demo.model.adm;
+
+public class User {
+    public int playerId;
+    public String playerName;
+}
---
+++ src/main/java/com/example/demo/model/adm/game.java
@@ -0,0 +1,12 @@
+package com.example.demo.model.adm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class game {
+    public String name;
+    public int id;
+    public boolean started;
+    public List<User> users = new ArrayList<>();
+
+}
