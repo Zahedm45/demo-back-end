08231cb16afe6e2f23844907a8bab04164a17634
Zahed <zahedm45@gmail.com>
2021-06-15 20:28:12 +0200

    createGame works

--- src/main/java/com/example/demo/DataLoader.java
+++ src/main/java/com/example/demo/DataLoader.java
@@ -23,54 +23,54 @@ public class DataLoader implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws ServiceException, DaoException {
-        Board board = new Board(8, 8, "Board1");
-        gameService.saveBoard(board);
-        Player player = new Player(board, "blue", "Player1");
-        gameService.addPlayer(board.getGameId(), player);
-        gameService.setCurrentPlayer(board.getGameId(), player.getPlayerId());
-        gameService.moveCurrentPlayer(board.getGameId(), 1, 1);
-        player = new Player(board, "green", "Player2");
-        gameService.addPlayer(board.getGameId(), player);
-        gameService.movePlayer(board, 4, 4, player.getPlayerId());
-        /*gameService.switchCurrentPlayer(board);*/
-
-
-
-
-        Board board2 = new Board(6, 6, "Board2");
-        gameService.saveBoard(board2);
-        Player player2 = new Player(board2, "yellow", "Player1");
-        gameService.addPlayer(board2.getGameId(), player2);
-        gameService.setCurrentPlayer(board2.getGameId(), player2.getPlayerId());
-        gameService.moveCurrentPlayer(board2.getGameId(), 1, 1);
-        player2 = new Player(board2, "blue", "Player2");
-        gameService.addPlayer(board2.getGameId(), player2);
-        gameService.movePlayer(board2, 4, 4, player2.getPlayerId());
-
-        player2 = new Player(board2, "red", "Player3");
-        gameService.addPlayer(board2.getGameId(), player2);
-        gameService.movePlayer(board2, 2, 4, player2.getPlayerId());
-
-
-
-
-
-
-        Board board3 = new Board(6, 6, "Board3");
-        gameService.saveBoard(board3);
-        Player player3 = new Player(board3, "yellow", "Player1");
-        gameService.addPlayer(board3.getGameId(), player3);
-        gameService.setCurrentPlayer(board3.getGameId(), player3.getPlayerId());
-        gameService.moveCurrentPlayer(board3.getGameId(), 1, 1);
-        player3 = new Player(board3, "blue", "Player2");
-        gameService.addPlayer(board3.getGameId(), player3);
-        gameService.movePlayer(board3, 0, 0, player3.getPlayerId());
-
-
-
-
-        Board board4 = new Board(6, 6, "Board4");
-        gameService.saveBoard(board4);
+//        Board board = new Board(8, 8, "Board1");
+//        gameService.saveBoard(board);
+//        Player player = new Player(board, "blue", "Player1");
+//        gameService.addPlayer(board.getGameId(), player);
+//        gameService.setCurrentPlayer(board.getGameId(), player.getPlayerId());
+//        gameService.moveCurrentPlayer(board.getGameId(), 1, 1);
+//        player = new Player(board, "green", "Player2");
+//        gameService.addPlayer(board.getGameId(), player);
+//        gameService.movePlayer(board, 4, 4, player.getPlayerId());
+//        /*gameService.switchCurrentPlayer(board);*/
+//
+//
+//
+//
+//        Board board2 = new Board(6, 6, "Board2");
+//        gameService.saveBoard(board2);
+//        Player player2 = new Player(board2, "yellow", "Player1");
+//        gameService.addPlayer(board2.getGameId(), player2);
+//        gameService.setCurrentPlayer(board2.getGameId(), player2.getPlayerId());
+//        gameService.moveCurrentPlayer(board2.getGameId(), 1, 1);
+//        player2 = new Player(board2, "blue", "Player2");
+//        gameService.addPlayer(board2.getGameId(), player2);
+//        gameService.movePlayer(board2, 4, 4, player2.getPlayerId());
+//
+//        player2 = new Player(board2, "red", "Player3");
+//        gameService.addPlayer(board2.getGameId(), player2);
+//        gameService.movePlayer(board2, 2, 4, player2.getPlayerId());
+//
+//
+//
+//
+//
+//
+//        Board board3 = new Board(6, 6, "Board3");
+//        gameService.saveBoard(board3);
+//        Player player3 = new Player(board3, "yellow", "Player1");
+//        gameService.addPlayer(board3.getGameId(), player3);
+//        gameService.setCurrentPlayer(board3.getGameId(), player3.getPlayerId());
+//        gameService.moveCurrentPlayer(board3.getGameId(), 1, 1);
+//        player3 = new Player(board3, "blue", "Player2");
+//        gameService.addPlayer(board3.getGameId(), player3);
+//        gameService.movePlayer(board3, 0, 0, player3.getPlayerId());
+//
+//
+//
+//
+//        Board board4 = new Board(6, 6, "Board4");
+//        gameService.saveBoard(board4);
 
 
     }
